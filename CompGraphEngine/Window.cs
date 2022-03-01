c2fc0ccf74fa01cff729ae843854808e8441fce8
BraveHunter2001 <64270977+BraveHunter2001@users.noreply.github.com>
2022-03-01 15:08:49 +0300

    Hello triangle. Lesson 1

--- CompGraphEngine/Window.cs
+++ CompGraphEngine/Window.cs
@@ -8,10 +8,50 @@ namespace CompGraphEngine
 {
     internal class Window : GameWindow
     {
+        private readonly float[] _vertices =
+       {
+            -0.5f, -0.5f, 0.0f, // Bottom-left vertex
+             0.5f, -0.5f, 0.0f, // Bottom-right vertex
+             0.0f,  0.5f, 0.0f  // Top vertex
+        };
+
+        private int _vertexBufferObject;
+        private int _vertexArrayObject;
+
+        private Shader _shader;
+
         public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) : base(gameWindowSettings, nativeWindowSettings)
         {
         }
 
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+            GL.ClearColor(0f, 0f, 0f, 1.0f);
+            //VBO input vertices
+            _vertexBufferObject = GL.GenBuffer();
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+            GL.BufferData(BufferTarget.ArrayBuffer, _vertices.Length * sizeof(float), _vertices, BufferUsageHint.StaticDraw);
+            // VAO
+            _vertexArrayObject = GL.GenVertexArray();
+            GL.BindVertexArray(_vertexArrayObject);
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+
+            GL.EnableVertexAttribArray(0);
+
+            _shader = new Shader("Shaders/shader.vert", "Shaders/shader.frag");
+            _shader.Use();
+        }
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+            _shader.Use();
+            GL.BindVertexArray(_vertexArrayObject);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+            SwapBuffers();
+        }
         protected override void OnUpdateFrame(FrameEventArgs args)
         {
             if ( KeyboardState.IsKeyDown(Keys.Escape))
@@ -19,6 +59,28 @@ namespace CompGraphEngine
                 Close(); 
             }
             base.OnUpdateFrame(args);
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, Size.X, Size.Y);
+        }
+
+        protected override void OnUnload()
+        {
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+
+            GL.UseProgram(0);
+
+            GL.DeleteBuffer(_vertexBufferObject);
+            GL.DeleteVertexArray(_vertexArrayObject);
+
+            GL.DeleteProgram(_shader.Handle);
+
+            base.OnUnload();
         }
     }
 }
