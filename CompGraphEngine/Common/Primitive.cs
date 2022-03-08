cf0a9257583ba7a4d1af09e292b7a875357a5108
BraveHunter2001 <64270977+BraveHunter2001@users.noreply.github.com>
2022-03-08 19:39:03 +0300

    init-pirm

---
+++ CompGraphEngine/Common/Primitive.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CompGraphEngine.Common
+{
+    public abstract class Primitive
+    {
+        private readonly float[] _vertices;
+        private readonly uint[] _indices;
+
+        private int _vertexBufferObject;
+        private int _vertexArrayObject;
+        private int _elementBufferObject;
+
+        private Shader _defaultShader;
+
+        
+    }
+}
---
+++ CompGraphEngine/Shaders/triangle.glsl
@@ -0,0 +1,19 @@
+﻿#type vertex
+#version 330 core
+
+layout(location = 0) in vec3 aPosition;
+void main(void)
+{
+     gl_Position = vec4(aPosition, 1.0); 
+     
+}
+
+#type fragment
+#version 330 core
+
+out vec4 outputColor;
+uniform vec4 ourColor;
+void main()
+{
+    outputColor = ourColor;
+}
